40416ba6a2f1a639445e86e902ba506fa15baa6d
rem-li <eliza.remli@gmail.com>
2023-02-18 15:29:37 +0300

    admin updated

--- hakaton/api/admin.py
+++ hakaton/api/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Category, Type
 # from .models import Ingredient
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('category', 'type', 'title', 'author')
+    list_display = ('category', 'type', 'title')
     search_fields = ('title', 'text',)
     empty_value_display = '-пусто-'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title')
+    search_fields = ('title')
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('title')
+    search_fields = ('title')
 
 
 # @admin.register(Ingredient)
