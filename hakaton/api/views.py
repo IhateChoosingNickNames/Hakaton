7657f6e2d649654bd015ec0062165f145ad388db
Natalya Shulgina <natulishka84@gmail.com>
2023-02-18 16:32:14 +0300

    view get and add

--- hakaton/api/views.py
+++ hakaton/api/views.py
@@ -4,7 +4,6 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def get_recipe(category, type=None, amount=5):
     '''Получает рецепты.'''
-    
     amount = int(amount)
     try:
         category = Category.objects.get(title=category.capitalize())
@@ -16,3 +15,18 @@ def get_recipe(category, type=None, amount=5):
     except ObjectDoesNotExist:
         return []
     return recipes[:amount]
+
+
+def add_recipe(category, type, title, text):
+    '''Добавляет рецепты.'''
+    title, text = title.strip(), text.strip()
+    if not title or not text:
+        return 404
+    try:
+        category = Category.objects.get(title=category.capitalize())
+        type = Type.objects.get(title=type.capitalize())
+        Recipe.objects.create(category=category, type=type, title=title, text=text)
+    except (ObjectDoesNotExist, Exception):
+        return 404
+    return 200
+    
