839578fc1a8ccdcbc0f8ef29187068fa1babafa6
Ларькин Михаил <jetpilot314@gmail.com>
2023-02-18 17:23:16 +0300

    Semi-final version

--- hakaton/api/models.py
+++ hakaton/api/models.py
@@ -1,16 +1,4 @@
-# from django.contrib.auth import get_user_model
 from django.db import models
-
-# User = get_user_model()
-
-
-# class Сuisine(models.Model):
-#    '''Класс кухня: европейская, русская, азиатская и т.д.'''
-#     title = models.CharField(max_length=256)
-#     slug = models.SlugField(max_length=50, unique=True)
-    
-#     def __str__(self):
-#         return self.title
 
 
 class Category(models.Model):
@@ -33,10 +21,7 @@ class Type(models.Model):
     
 class Recipe(models.Model):
     '''Класс рецепты'''
-    
-    # cuisine = models.ForeignKey(
-    #     Сuisine, on_delete=models.SET_NULL, related_name='recipies'
-    # )
+
     category = models.ForeignKey(
         Category, verbose_name='Категории', on_delete=models.CASCADE, related_name='recipies'
     )
@@ -45,12 +30,7 @@ class Recipe(models.Model):
     )
     title = models.CharField(max_length=200, verbose_name='Название')
     text = models.TextField(verbose_name='Описание рецепта')
-    # author = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, related_name='recipies'
-    # )
-    # score = models.IntegerField(
-    #     validators=[MinValueValidator(1), MaxValueValidator(10)]
-    # )
+
     pub_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     # image = models.ImageField(
     #     upload_to='hakaton/', null=True, blank=True)
@@ -60,29 +40,3 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
-
-
-# class Ingredient(models.Model):
-#     title = models.CharField(max_length=256)
-#     slug = models.SlugField(max_length=50, unique=True)
-    
-#     def __str__(self):
-#         return self.title
-
-
-# class RecipeIngredient(models.Model):
-#     recipe = models.ForeignKey(
-#         Recipe,
-#         on_delete=models.CASCADE,
-#         related_name='recipe'
-#     )
-#     ingredient = models.ForeignKey(
-#         Ingredient,
-#         on_delete=models.CASCADE,
-#         related_name='ingredient'
-#     )
-#     class Meta:
-#         constraints = [
-#             models.UniqueConstraint(fields=['recipe', 'ingredient'],
-#                                     name='unique ingredient')
-#         ]
