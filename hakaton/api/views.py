4aeb2694bdc6dd16c7ee154768dce630da35cbcb
Natalya Shulgina <natulishka84@gmail.com>
2023-02-18 19:35:37 +0300

    author new

--- hakaton/api/views.py
+++ hakaton/api/views.py
@@ -1,4 +1,4 @@
-from api.models import Category, Recipe, Type
+from api.models import Author, Category, Recipe, Type
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -17,15 +17,26 @@ def get_recipe(category, type=None, amount=5):
     return recipes[:amount]
 
 
-def add_recipe(category, type, title, text):
+def add_recipe(category, type, title, text, author_id):
     '''Добавляет рецепты.'''
     title, text = title.strip(), text.strip()
     if not title or not text:
         return 404
+    author, created = Author.objects.get_or_create(author_id = author_id)
     try:
         category = Category.objects.get(title=category.capitalize())
         type = Type.objects.get(title=type.capitalize())
-        Recipe.objects.create(category=category, type=type, title=title, text=text)
-    except (ObjectDoesNotExist, Exception):
+        Recipe.objects.create(category=category, type=type, title=title, text=text, author=author)
+    except Exception:
         return 404
     return 200
+    
+
+def get_my_recipes(author_id):
+    '''Ищет свои рецепты.'''
+    try:
+        author = Author.objects.get(author_id = author_id)
+        recipes = author.recipies.all()
+    except ObjectDoesNotExist:
+        return []
+    return recipes
