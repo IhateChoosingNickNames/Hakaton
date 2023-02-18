842c6142c8df629551c1d208c273622f9d8b340d
Ларькин Михаил <jetpilot314@gmail.com>
2023-02-18 15:53:20 +0300

    Improved bot, views.

--- hakaton/api/views.py
+++ hakaton/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from api.models import Recipe
 
-def get_recipe(cat, couisin=None, amount=5):
-    return Recipe.objects.all()
+def get_reciper(cat, couisin=None, amount=5):
+    return Recipe.objects.all()[:amount]
