105678de6312041d09bf6ee7ff920dbb162f4910
Elizaveta Veremeva <79760342+rem-li@users.noreply.github.com>
2023-02-18 17:35:34 +0600

    Update admin.py

--- hakaton/api/admin.py
+++ hakaton/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Recipe
+# from .models import Ingredient
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('category', 'type', 'title', 'author')
+    search_fields = ('title', 'text',)
+    empty_value_display = '-пусто-'
+
+
+# @admin.register(Ingredient)
+# class IngredientAdmin(admin.ModelAdmin):
+#    list_display = ('title')
+#    search_fields = ('title',)
+#    empty_value_display = '-пусто-'
